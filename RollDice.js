6d398236779eb4ec17c903151238f7696f2655d9
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2022-08-09 09:01:42 +0100

    Update to use shadow DOM

--- RollDice.js
+++ RollDice.js
@@ -28,15 +28,15 @@ export default class RollDice extends HTMLElement {
 
   constructor() {
     super();
-    const btnText = this.textContent.trim();
-    this.innerHTML = /* html */ `
-      <p><button type="button">${btnText || "Roll Dice"}</button></p>
+    this.root = this.attachShadow({ mode: "closed" });
+    this.root.innerHTML = /* html */ `
+      <p><button type="button"><slot>Roll Dice</slot></button></p>
       <div aria-live="polite"></div>
     `;
   }
 
   #handleClick = () => {
-    const liveRegion = this.querySelector("[aria-live]");
+    const liveRegion = this.root.querySelector("[aria-live]");
     if (!liveRegion) return;
 
     const [roll] = RollDice.#shuffle(RollDice.#die);
@@ -44,13 +44,13 @@ export default class RollDice extends HTMLElement {
   };
 
   connectedCallback() {
-    const btn = this.querySelector("button");
+    const btn = this.root.querySelector("button");
     if (!btn) return;
     btn.addEventListener("click", this.#handleClick);
   }
 
   disconnectedCallback() {
-    const btn = this.querySelector("button");
+    const btn = this.root.querySelector("button");
     if (!btn) return;
     btn.removeEventListener("click", this.#handleClick);
   }
